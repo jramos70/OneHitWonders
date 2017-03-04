98e090139a68da873f9073b448fcc485c2a3f207
chaudhrya <abraarzchaudhry@gmail.com>
2017-03-03 19:21:34 -0500

    genre improvement
    uses spotify if last.fm fails, returns NULL if both fail, may want to
    reduce the sleep time, because it would take ~9hrs to all the songs at
    this rate, not sure what the limit rate is, but this shouldn't throw any
    errors and it should work for nearly all songs

--- OneHit/genre.py
+++ OneHit/genre.py
@@ -6,9 +6,21 @@ def genre(artist,song):
     url = 'http://ws.audioscrobbler.com/2.0/?method=track.gettoptags&artist='
     artstr = '+'.join(artist.lower().split())
     songstr = '+'.join(song.lower().split())
-    url += artstr + '&track=' + songstr + '&api_key=' + api_key + '&format=json'
+    url += artstr + '&track=' + songstr + '&autocorrect=1' \
+        + '&api_key=' + api_key + '&format=json'
     j = requests.get(url).json()
     time.sleep(0.3)
+    if ('toptags' not in j) or (len(j['toptags']['tag']) == 0):
+        return spotify_genre(artstr)
     return j['toptags']['tag'][0]['name']
 
-print genre('Ed Sheeran','Shape Of You')
+def spotify_genre(artist):
+    url = 'https://api.spotify.com/v1/search?q=' + artist + '&type=artist'
+    j = requests.get(url).json()
+    if len(j['artists']['items']) == 0:
+        return 'NULL'
+    if len(j['artists']['items'][0]['genres']) == 0:
+        return 'NULL'
+    return j['artists']['items'][0]['genres'][0]
+
+print genre('zayn','I Don\'t Wanna Live Forever (Fifty Shades Darker)')
