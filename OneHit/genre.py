33912b8d8e06e6b0fddb425f941abad439016b6a
chaudhrya <abraarzchaudhry@gmail.com>
2017-03-20 18:04:20 -0400

    genre update
    now works with a list of defined genres

--- OneHit/genre.py
+++ OneHit/genre.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-def genre(artist,song):
+def genre(artist, song, genre_list):
     api_key = 'b45db8136c5b56cfa6aa2b77fe6c3a61'
     url = 'http://ws.audioscrobbler.com/2.0/?method=track.gettoptags&artist='
     artstr = '+'.join(artist.lower().split())
@@ -11,16 +11,24 @@ def genre(artist,song):
     j = requests.get(url).json()
     time.sleep(0.3)
     if ('toptags' not in j) or (len(j['toptags']['tag']) == 0):
-        return spotify_genre(artstr)
-    return j['toptags']['tag'][0]['name']
+        return spotify_genre(artstr, genre_list)
+    for tag in map(lambda x: x['name'].lower(), j['toptags']['tag']):
+        for genre in genre_list:
+            if genre in tag:
+                return genre
+    return spotify_genre(artstr, genre_list)
 
-def spotify_genre(artist):
+def spotify_genre(artist, genre_list):
     url = 'https://api.spotify.com/v1/search?q=' + artist + '&type=artist'
     j = requests.get(url).json()
     if len(j['artists']['items']) == 0:
         return 'NULL'
     if len(j['artists']['items'][0]['genres']) == 0:
         return 'NULL'
-    return j['artists']['items'][0]['genres'][0]
+    for tag in map(lambda x: x.lower(), j['artists']['items'][0]['genres']):
+        for genre in genre_list:
+            if genre in tag:
+                return genre
+    return 'NULL'
 
-print genre('zayn','I Don\'t Wanna Live Forever (Fifty Shades Darker)')
+print(genre('ed sheeran','shape of you',['pop']))
