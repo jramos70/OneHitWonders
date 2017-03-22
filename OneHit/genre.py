61c82fd0296dde8f6710678a270206fb6f13e5a4
jramos70 <Jerome@Jeromes-MacBook-Pro.local>
2017-03-22 18:25:11 -0400

    Fixed genre functionality

--- OneHit/genre.py
+++ OneHit/genre.py
@@ -20,7 +20,6 @@ Dubstep
 Techno
 Reggae
 Instrumental
-<<<<<<< HEAD
 Emo
 Crunk
 Hollywood
@@ -31,15 +30,6 @@ genre_list.sort(key=len)
 genre_list.reverse()
 
 def genre(artist, song):
-=======
-K-pop
-Emo
-Crunk
-Raggaeton
-
-genre_list = list(map(lambda x: x.strip().lower(), genre_list_raw.split()))
-
-def genre(artist, song, genre_list):
     api_key = 'b45db8136c5b56cfa6aa2b77fe6c3a61'
     url = 'http://ws.audioscrobbler.com/2.0/?method=track.gettoptags&artist='
     artstr = '+'.join(artist.lower().split())
@@ -50,15 +40,13 @@ def genre(artist, song, genre_list):
     time.sleep(0.2)
     if ('toptags' not in j) or (len(j['toptags']['tag']) == 0):
         return spotify_genre(artstr)
-        return spotify_genre(artstr, genre_list)
     for tag in map(lambda x: x['name'].lower(), j['toptags']['tag']):
         for genre in genre_list:
             if genre in tag:
                 return genre
     return spotify_genre(artstr)
-    return spotify_genre(artstr, genre_list)
 
-def spotify_genre(artist, genre_list):
+def spotify_genre(artist):
     url = 'https://api.spotify.com/v1/search?q=' + artist + '&type=artist'
     j = requests.get(url).json()
     if len(j['artists']['items']) == 0:
@@ -70,13 +58,3 @@ def spotify_genre(artist, genre_list):
             if genre in tag:
                 return genre
     return 'NULL'
-# print(genre('Ed Sheeran','Shape Of You', genre_list)) #pop
-# print(genre('Migos','Bad and Boujee', genre_list)) #rap
-# print(genre('Marian Hill','Down', genre_list)) #electronic
-# print(genre('Keith Urban','The Fighter', genre_list)) #country
-
-
-print(genre('Ed Sheeran','Shape Of You', genre_list)) #pop
-print(genre('Migos','Bad and Boujee', genre_list)) #rap
-print(genre('Marian Hill','Down', genre_list)) #electronic
-print(genre('Keith Urban','The Fighter', genre_list)) #country
