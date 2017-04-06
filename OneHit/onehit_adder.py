ebb9c924e8986c25de67066afb8e6eddc97dad77
jramos70 <Jerome@Jeromes-MacBook-Pro.local>
2017-04-05 22:40:38 -0400

    Added if song is one hit or not

---
+++ OneHit/onehit_adder.py
@@ -0,0 +1,57 @@
+import csv
+import itertools
+import re
+
+hits = {}
+one_hit_checker = {}
+month_dict= {'Jan': 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
+with open ('oneWiki.csv', 'r', encoding = 'latin1') as r:
+    reader = csv.reader(r)
+    next(reader, None)
+    for row in reader:
+        song = row[1][1:-1]
+        peak = row[2]
+        date = row[3].split("-")
+        month = str(month_dict[date[1]])
+        day = date[0]
+        year = row[4][2:]
+        hits[(peak, month, day, year)] = song
+
+with open('top100_billboard.csv', 'r', encoding = 'utf-8') as r:
+    reader = csv.reader(r)
+    next(reader, None)
+    for row in reader:
+        peak = row[5]
+        date = row[0].split("/")
+        month = date[0]
+        day = date[1]
+        year = date[2]
+        check = (peak, month, day, year)
+        if check in hits:
+            one_hit_checker[(row[2], row[3])] = "True"
+    
+with open ('one_hit_wonder.csv','w', encoding = 'utf-8') as f:
+    with open('top100_billboard.csv', 'r', encoding = 'utf-8') as r:
+        writer = csv.writer(f)
+        reader = csv.reader(r)
+        next(reader, None)
+        writer.writerow(['date', 'position', 'songname', 'artist', 'last week', 'peak position', 'weeks on chart', 'genre', 'isOneHit'])
+        for row in reader:
+            isOneHit = "False"
+            peak = row[5]
+            date = row[0].split("/")
+            month = date[0]
+            day = date[1]
+            year = date[2]
+            check = (peak, month, day, year)
+            
+            if ((row[2], row[3]) in one_hit_checker):
+                isOneHit = "True"
+                
+            writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], isOneHit])
+
+        
+
+
+
+        
